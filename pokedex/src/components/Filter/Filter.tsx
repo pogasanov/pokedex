b9682c30175d19452fdc8bad756695587bf03505
Pavel <pogasanov@gmail.com>
2022-10-26 16:16:12 +0600

    submit selected filters

--- pokedex/src/components/Filter/Filter.tsx
+++ pokedex/src/components/Filter/Filter.tsx
@@ -1,27 +1,58 @@
+import {FormEvent, useState} from "react";
 import {ISpecies, IType} from "types";
 
 interface IProps {
   types: IType[]
   species: ISpecies[]
+  onChange: (typesIds: number[], speciesId: number) => void
 }
 
-export default function Filter({types, species}: IProps) {
+export default function Filter({types, species, onChange}: IProps) {
+  const [selectedSpecies, setSelectedSpecies] = useState<number>(0)
+  const [selectedTypes, setSelectedTypes] = useState<number[]>([])
+  const toggleSelectedType = (id: number) => {
+    const arrayCopy = [...selectedTypes]
+    const index = arrayCopy.indexOf(id);
+    if (index === -1) {
+      arrayCopy.push(id);
+    } else {
+      arrayCopy.splice(index, 1);
+    }
+    setSelectedTypes(arrayCopy)
+  }
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    onChange(selectedTypes, selectedSpecies)
+  }
+
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <div>
         {types.map(type => (
-          <label key={type.id} htmlFor={`type-${type.id}`}>{type.name}
-            <input type="checkbox" name="type" id={`type-${type.id}`} value={type.id}/>
-          </label>
+          <div key={type.id}>
+            <label htmlFor={`type-${type.id}`}>{type.name}
+              <input
+                type="checkbox"
+                name="type"
+                id={`type-${type.id}`}
+                value={type.id}
+                checked={selectedTypes.includes(type.id)}
+                onChange={e => toggleSelectedType(Number(e.target.value))}
+              />
+            </label>
+          </div>
         ))}
       </div>
       <div>
         <label htmlFor="species">Species</label>
-        <select name="species" id="species">
-          <option value=""></option>
+        <select name="species" id="species" value={selectedSpecies}
+                onChange={e => setSelectedSpecies(Number(e.target.value))}>
+          <option value="0"></option>
           {species.map(sp => <option key={sp.id} value={sp.id}>{sp.name}</option>)}
         </select>
       </div>
-    </div>
+      <button type="submit">Submit</button>
+    </form>
   )
 }
